df55a4242340fcfed09e76a63dafbab8614cdf3a
rqthomas <rqthomas@vt.edu>
2021-06-03 18:31:24 +0000

    updates for rcc

--- calculatePhenoCamUncertainty.R
+++ calculatePhenoCamUncertainty.R
@@ -4,7 +4,7 @@
 ##' @param dates Vector of desired dates to calculate standard deviation for
 ##' @export
 calculate.phenocam.uncertainty <- function(dat,dates) {
-  sds <- rep(NA,length(dates))
+  sds <- array(NA,dim = c(length(dates), 2))
   nboot <- 50
   for(d in 1:length(dates)){
     dailyDat <- dat[dat$date==dates[d],]
@@ -12,12 +12,16 @@ calculate.phenocam.uncertainty <- function(dat,dates) {
       dailyDat <- dailyDat[!is.na(dailyDat$gcc),]
       nrows <- nrow(dailyDat)
       gcc_90s <- rep(NA,nboot)
+      rcc_90s <- rep(NA,nboot)
       for(j in 1:nboot){
         gcc_90s[j] <- quantile(dailyDat$gcc[sample(x = 1:nrows,size = nrows,replace = T)],0.90)
+        rcc_90s[j] <- quantile(dailyDat$rcc[sample(x = 1:nrows,size = nrows,replace = T)],0.90)
       }
-      sds[d] <- sd(gcc_90s)
+      sds[d,1] <- sd(gcc_90s)
+      sds[d,2] <- sd(rcc_90s)
     }else{
-      sds[d] <- NA
+      sds[d, 1] <- NA
+      sds[d, 2] <- NA
     }
   }
   return(sds)
