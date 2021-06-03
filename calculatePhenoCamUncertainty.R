6abad90b254f70ffabfd654d50c1124f0e63419b
Michael Dietze <dietze@bu.edu>
2021-06-03 09:41:39 -0400

    generalize phenocam uncertainty to any band, set gcc as default for backward compatibility

--- calculatePhenoCamUncertainty.R
+++ calculatePhenoCamUncertainty.R
@@ -3,17 +3,17 @@
 ##' @param dat PhenoCam data dataframe from roistats file
 ##' @param dates Vector of desired dates to calculate standard deviation for
 ##' @export
-calculate.phenocam.uncertainty <- function(dat,dates) {
+calculate.phenocam.uncertainty <- function(dat,dates,target="gcc") {
   sds <- rep(NA,length(dates))
   nboot <- 50
   for(d in 1:length(dates)){
     dailyDat <- dat[dat$date==dates[d],]
     if(nrow(dailyDat)>0){
-      dailyDat <- dailyDat[!is.na(dailyDat$gcc),]
+      dailyDat <- dailyDat[!is.na(dailyDat[,target]),]
       nrows <- nrow(dailyDat)
       gcc_90s <- rep(NA,nboot)
       for(j in 1:nboot){
-        gcc_90s[j] <- quantile(dailyDat$gcc[sample(x = 1:nrows,size = nrows,replace = T)],0.90)
+        gcc_90s[j] <- quantile(dailyDat[,target][sample(x = 1:nrows,size = nrows,replace = T)],0.90)
       }
       sds[d] <- sd(gcc_90s)
     }else{
