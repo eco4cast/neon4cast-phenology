87ea5e719b23478f897402e43f6b351b593fbdf2
rqthomas <rqthomas@vt.edu>
2022-08-05 13:32:41 +0000

    update to nulls

--- phenology_climatology.R
+++ phenology_climatology.R
@@ -11,14 +11,11 @@ sites <- read_csv("Phenology_NEON_Field_Site_Metadata_20210928.csv")
 
 target_clim <- target %>%  
   mutate(doy = yday(time)) %>% 
-  group_by(doy, siteID) %>% 
-  summarise(gcc_clim = mean(gcc_90, na.rm = TRUE),
-            rcc_clim = mean(rcc_90, na.rm = TRUE),
-            gcc_sd = sd(gcc_90, na.rm = TRUE),
-            rcc_sd = sd(rcc_90, na.rm = TRUE),
+  group_by(doy, site_id, variable) %>% 
+  summarise(mean = mean(observed, na.rm = TRUE),
+            sd = mean(observed, na.rm = TRUE),
             .groups = "drop") %>% 
-  mutate(gcc_clim = ifelse(is.nan(gcc_clim), NA, gcc_clim),
-         rcc_clim = ifelse(is.nan(rcc_clim), NA, rcc_clim))
+  mutate(mean = ifelse(is.nan(mean), NA, mean))
 
 #curr_month <- month(Sys.Date())
 curr_month <- month(Sys.Date())
@@ -39,46 +36,42 @@ forecast <- target_clim %>%
                                as_date((doy-1), origin = paste(year(Sys.Date())+1, "01", "01", sep = "-")),
                                as_date((doy-1), origin = paste(year(Sys.Date()), "01", "01", sep = "-")))))
 
-subseted_site_names <- unique(forecast$siteID)
+subseted_site_names <- unique(forecast$site_id)
 site_vector <- NULL
 for(i in 1:length(subseted_site_names)){
   site_vector <- c(site_vector, rep(subseted_site_names[i], length(forecast_dates)))
 }
 
-forecast_tibble <- tibble(time = rep(forecast_dates, length(subseted_site_names)),
-                          siteID = site_vector)
+forecast_tibble1 <- tibble(time = rep(forecast_dates, length(subseted_site_names)),
+                          site_id = site_vector,
+                          variable = "gcc_90")
 
-gcc_90 <- forecast %>% 
-  select(time, siteID, gcc_clim, gcc_sd) %>% 
-  rename(mean = gcc_clim,
-         sd = gcc_sd) %>% 
-  group_by(siteID) %>% 
+forecast_tibble2 <- tibble(time = rep(forecast_dates, length(subseted_site_names)),
+                          site_id = site_vector,
+                          variable = "rcc_90")
+
+forecast_tibble <- bind_rows(forecast_tibble1, forecast_tibble2)
+
+
+
+combined <- forecast %>% 
+  select(time, site_id, mean, sd, variable) %>% 
+  group_by(site_id, variable) %>% 
   mutate(mean = imputeTS::na_interpolation(mean, maxgap = 3),
          sd = median(sd, na.rm = TRUE)) %>%
-  pivot_longer(c("mean", "sd"),names_to = "statistic", values_to = "gcc_90")
-
-rcc_90 <- forecast %>% 
-  select(time, siteID, rcc_clim, rcc_sd) %>% 
-  rename(mean =rcc_clim,
-         sd = rcc_sd) %>% 
-  group_by(siteID) %>% 
-  mutate(mean = imputeTS::na_interpolation(mean, maxgap = 3),
-         sd = median(sd, na.rm = TRUE)) %>%
-  pivot_longer(c("mean", "sd"),names_to = "statistic", values_to = "rcc_90")
-
-combined <- full_join(gcc_90, rcc_90) %>%  
-  mutate(data_assimilation = 0,
-         forecast = 1) %>% 
-  select(time, siteID, statistic, forecast, gcc_90, rcc_90) %>% 
-  arrange(siteID, time, statistic) 
+  pivot_longer(c("mean", "sd"),names_to = "parameter", values_to = "predicted") |> 
+  mutate(family = "norm") |> 
+  arrange(site_id, time, statistic) |> 
+  select(time, site_id, variable, family, parameter, predicted)
 
 combined %>% 
-  select(time, gcc_90 ,statistic, siteID) %>% 
-  pivot_wider(names_from = statistic, values_from = gcc_90) %>% 
+  filter(variable == "gcc_90") |> 
+  select(time, site_id,parameter, predicted) %>% 
+  pivot_wider(names_from = parameter, values_from = predicted) %>% 
   ggplot(aes(x = time)) +
   geom_ribbon(aes(ymin=mean - sd*1.96, ymax=mean + sd*1.96), alpha = 0.1) + 
   geom_point(aes(y = mean)) +
-  facet_wrap(~siteID)
+  facet_wrap(~site_id)
 
 forecast_file <- paste("phenology", min(combined$time), "climatology.csv.gz", sep = "-")
 
@@ -126,7 +119,7 @@ meta_data_filename <- neon4cast::generate_metadata(forecast_file = forecast_file
                                                    model_metadata = model_metadata)
 
 neon4cast::submit(forecast_file = forecast_file, 
-                  metadata = meta_data_filename, 
+                  metadata = NULL, 
                   ask = FALSE)
 
 unlink(forecast_file)
